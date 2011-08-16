c396c9b2d19b08810fe0f594f30b891e39bb7e01
Yvon Pharand <yvon.pharand@accenture.com>
2011-08-16 10:42:24 -0400

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '1f73618f79cf932f8b7969d43b6958dadeeb580c17b251c622d946c3cec1a69abccbef897aef32b17f1630ff34a55331df3602db3502c2e19eb5eac8329434c7'
+DemoApp::Application.config.secret_token = '395046a527dc6c595924ff72cdeb8fa814f8729f8e0c6acf4c73c1cb6ad2831d89f3e66d3843a906ef34cea511e71e950d855c88e8d43928f5c5d29c0f9021b0'
